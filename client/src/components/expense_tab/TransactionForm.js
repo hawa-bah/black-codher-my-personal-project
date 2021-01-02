4ff57a33b4cad9d5c408ed2762c34eb015520b80
Hawa BahBah <hawabahbah25@gmail.com>
2021-01-02 04:30:08 +0000

    creating new form component

--- client/src/components/expense_tab/TransactionForm.js
+++ client/src/components/expense_tab/TransactionForm.js
@@ -1,68 +1,7 @@
 import React from "react";
 
-
-
-  return (
-    <DatePicker
-      {...rest}
-      name={name}
-      helperText={showError ? meta.error || meta.submitError : undefined}
-      error={showError}
-      inputProps={restInput}
-      onChange={onChange}
-      value={value === "" ? null : value}
-    />
-  );
-}
-
-function TimePickerWrapper(props) {
-  const {
-    input: { name, onChange, value, ...restInput },
-    meta,
-    ...rest
-  } = props;
-  const showError =
-    ((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) &&
-    meta.touched;
-
-  return (
-    <TimePicker
-      {...rest}
-      name={name}
-      helperText={showError ? meta.error || meta.submitError : undefined}
-      error={showError}
-      inputProps={restInput}
-      onChange={onChange}
-      value={value === "" ? null : value}
-    />
-  );
-}
-
-const onSubmit = async (values) => {
-  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  await sleep(300);
-  window.alert(JSON.stringify(values, 0, 2));
-};
-const validate = (values) => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  }
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  }
-  if (!values.email) {
-    errors.email = "Required";
-  }
-  return errors;
-};
-
 const TransactionForm = () => {
-  return (
-    <div style={{ padding: 16, margin: "auto", maxWidth: 600 }}>
-      
-    </div>
-  );
+  return <div style={{ padding: 16, margin: "auto", maxWidth: 600 }}></div>;
 };
 
 export default TransactionForm;
