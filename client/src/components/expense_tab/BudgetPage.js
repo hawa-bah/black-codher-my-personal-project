d5b82534f16cd582a832584480630706e10f9e52
Hawa BahBah <hawabahbah25@gmail.com>
2021-01-02 12:46:32 +0000

    creating material-ui form

--- client/src/components/expense_tab/BudgetPage.js
+++ client/src/components/expense_tab/BudgetPage.js
@@ -1,22 +1,82 @@
 import DateFnsUtils from "@date-io/date-fns";
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Col, Row } from "react-bootstrap";
 import axios from "axios";
 import TransactionsList from "../transaction_list/TransactionsList";
-// import TransactionForm from "../expense_tab/TransactionForm";
+import TransactionForm from "../expense_tab/TransactionForm";
 import { getBalance } from "../../services/transactionService";
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider,
 } from "@material-ui/pickers";
+
+import NumberFormat from "react-number-format";
+// import { Form, Field } from "react-final-form";
+
+import { Grid, MenuItem, TextField } from "@material-ui/core";
+// Material-ui
+import {
+  createMuiTheme,
+  withStyles,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { purple } from "@material-ui/core/colors";
 import budgetCategoriesArry from "../../budgetCategoriesArray";
 import BudgetCategories from "../BudgetCatgories";
 import "./BudgetPage.css";
 import "../../App.css";
 
+function NumberFormatCustom(props) {
+  const { inputRef, onChange, ...other } = props;
+
+  return (
+    <NumberFormat
+      {...other}
+      getInputRef={inputRef}
+      onValueChange={(values) => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value,
+          },
+        });
+      }}
+      thousandSeparator
+      isNumericString
+      prefix="£"
+    />
+  );
+}
+
+NumberFormatCustom.propTypes = {
+  inputRef: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    "&:hover": {
+      backgroundColor: purple[700],
+    },
+  },
+}))(Button);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
 const BudgetPage = (props) => {
+  const classes = useStyles();
   // >>>> I'm passing transactions state (the value of a transaction) as a props
   // const [budget, setbudget] = useState(0);
   // const [transaction, setTransactions] = useState(0);
@@ -47,8 +107,8 @@ const BudgetPage = (props) => {
     axios.post(`/api/expense`, {
       transaction_value: props.transaction,
       description: props.description,
-      transaction_date: selectedDate,
-      budget_category: transactionCategory,
+      transaction_date: selectedDate, //
+      budget_category: transactionCategory, //
       trip_name: tripTransaction,
     });
     //>>>> this is to clear the input fields once clicked submit. The values will still be saved in the mongo database
@@ -77,8 +137,112 @@ const BudgetPage = (props) => {
         {/* <h2>Amount spent: {balance}</h2>
         {console.log(balance)} */}
       </div>
+      <div
+        className="new-form-ui"
+        style={{ padding: 16, margin: "auto", maxWidth: 600 }}
+      >
+        <form
+          onSubmit={(event) => {
+            handleSubmit(event);
+            console.log("clickSubmit");
+            // renderBalance();
+          }}
+        >
+          <Grid container alignItems="flex-start" spacing={2}>
+            <Grid item xs={4}>
+              <TextField
+                id="Description"
+                color="secondary"
+                label="Description"
+                value={props.description}
+                onChange={(e) => props.setDesc(e.target.value)}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                id="TripName"
+                color="secondary"
+                label="Trip Name"
+                value={tripTransaction}
+                onChange={(e) => setTripTransaction(e.target.value)}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                label="Transaction value"
+                value={props.transaction}
+                onChange={(event) => props.setTransactions(event.target.value)}
+                name="Transaction-value-input"
+                id="Transaction-value-input"
+                InputProps={{
+                  inputComponent: NumberFormatCustom,
+                }}
+                fullwidth
+                required
+              />
+            </Grid>
+          </Grid>
+          <Grid container alignItems="flex-start" spacing={2}>
+            <Grid item xs={6}>
+              {/* to input the date of the transaction we are using material-ui */}
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  label="Date of the transaction"
+                  clearable
+                  value={selectedDate}
+                  placeholder="10/10/2018"
+                  onChange={(date) => handleDateChange(date)}
+                  // minDate={new Date()}
+                  format="MM/dd/yyyy"
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="category-input-form"
+                label="Select a category"
+                value={transactionCategory}
+                select
+                onChange={(event) => setTransactionCategory(event.target.value)}
+                style={{ width: "25ch" }}
+              >
+                {budgetCategoriesArry.map((category) => (
+                  <MenuItem key={category} value={category}>
+                    {category}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+          </Grid>
+          <ColorButton
+            variant="contained"
+            color="primary"
+            className={classes.margin}
+            type="submit"
+          >
+            Submit Form
+          </ColorButton>
+        </form>
+      </div>
       <div className="SubmitTransaction-Form-div">
-        {/* <TransactionForm></TransactionForm> */}
+        {/* <TransactionForm
+          handleSubmit={handleSubmit}
+          description={props.description}
+          setDesc={props.setDesc}
+          tripTransaction={tripTransaction}
+          setTripTransaction={setTripTransaction}
+          transactionCategory={transactionCategory}
+          setTransactionCategory={setTransactionCategory}
+          selectedDate={selectedDate}
+          handleDateChange={handleDateChange}
+          //
+          hasSubmitedTransaction={hasSubmitedTransaction}
+          setHasSubmitedTransaction={setHasSubmitedTransaction}
+        ></TransactionForm> */}
         <h2>Submit a transaction</h2>
         <Form
           onSubmit={(event) => {
