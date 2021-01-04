700cc0c7c1deb08a5fc359c7150debbec4f92e69
Hawa BahBah <hawabahbah25@gmail.com>
2021-01-04 18:23:13 +0000

    InfoCards are displayed

---
+++ client/src/components/material-ui/Button.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+// Material-ui
+
+import { Button } from "@material-ui/core";
+import {
+  createMuiTheme,
+  withStyles,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { purple } from "@material-ui/core/colors";
+import EditIcon from "@material-ui/icons/Edit";
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    "&:hover": {
+      backgroundColor: purple[700],
+    },
+  },
+}))(Button);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
--- client/src/pages/SubmitBudgets.js
+++ client/src/pages/SubmitBudgets.js
@@ -1,60 +1,88 @@
 import react, { useEffect, useState } from "react";
-import { Grid, MenuItem, TextField, Button } from "@material-ui/core";
 import axios from "axios";
 import { getAll } from "../services/budgetService";
 
+// Material-ui
+import { Grid, MenuItem, TextField, Button } from "@material-ui/core";
+import {
+  createMuiTheme,
+  withStyles,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { purple } from "@material-ui/core/colors";
+import EditIcon from "@material-ui/icons/Edit";
+
+// MATERIAL-UI:
+const EditButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    "&:hover": {
+      backgroundColor: purple[700],
+    },
+    borderRadius: "20px",
+    maxWidth: "fit-content",
+    position: "left",
+  },
+}))(Button);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(0),
+  },
+}));
+
 const SubmitBudgetPage = (props) => {
-  const [budget, setBudget] = useState(null);
-  const [budgetAmount, setBudgetAmount] = useState(null);
+  const classes = useStyles;
+  const [infoCards, setInfoCards] = useState([]);
+  const [hasSubmitedInfo, setHasSubmitedInfo] = useState(false);
+  const [clickEdit, setClickEdit] = useState(false);
+  const [editCard, setEditCard] = useState({});
+
   const [tripName, setTripName] = useState("");
 
-  const [budgetArray, setBudgetArray] = useState([]);
-  const [hasSubmited, setHasSubmited] = useState(false);
-
-  const [infoCards, setInfoCards] = useState([]);
-
   useEffect(() => {
-    getInfoCards();
+    getInfoCards(); // this by itself causes an infinite loop but solve if useEffect is only called once
+  }, [hasSubmitedInfo]);
 
-    if (hasSubmited) {
-      setBudgetArray([]);
-      setTripName("");
-      setHasSubmited(false);
-    }
-    if (budgetArray) {
-      handleSubmitBudget();
-    }
-  }, [infoCards]);
+  const handlesubmitInfo = () => {
+    setHasSubmitedInfo(!hasSubmitedInfo);
+  };
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    handleSubmitBudget();
-  }
+  const handleClickEdit = (infoCard) => {
+    console.log(infoCard);
+    setClickEdit(true);
+    setEditCard(infoCard);
+    // return (
+    //   <form className="info-form-edit" style={{ position: "relative" }}>
+    //     <Grid>
+    //       <TextField
+    //         id="Trip Name"
+    //         color="secondary"
+    //         label="Trip Name"
+    //         value={infoCard.trip_name}
+    //       />
+    //     </Grid>
+    //   </form>
+    // );
+  };
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   handleSubmitBudget();
+  // }
 
-  function handleSubmitBudget() {
-    const array = [];
-    const obj = {
-      budget_category: budget,
-      budget_amount: budgetAmount,
-    };
-    budgetArray.push(obj);
-    setBudgetArray(budgetArray);
-    console.log(budgetArray);
-  }
+  // function handleSubmitBudget() {
+  //   const array = [];
+  //   const obj = {
+  //     budget_category: budget,
+  //     budget_amount: budgetAmount,
+  //   };
+  //   budgetArray.push(obj);
+  //   setBudgetArray(budgetArray);
+  //   console.log(budgetArray);
+  // }
 
-  function handlePostInfo() {
-    console.log({
-      budgets: budgetArray,
-      trip_name: tripName,
-    });
-
-    axios.post(`/api/budget`, {
-      budgets: budgetArray,
-      trip_name: tripName,
-    });
-
-    setHasSubmited(true);
-  }
   const getInfoCards = async () => {
     //repeated code >>>> I am getting the documents from the budget collection whith budgetService.js
     let res = await getAll();
@@ -64,57 +92,63 @@ const SubmitBudgetPage = (props) => {
     console.log(infoCards);
   };
 
+  const renderInfoCard = (infoCard) => {
+    return (
+      <div className="info-card-item">
+        <h2 style={{ position: "absolute" }}>{infoCard.trip_name}</h2>
+        <EditButton
+          variant="contained"
+          color="primary"
+          label=""
+          className={classes.margin}
+          startIcon={<EditIcon />}
+          onClick={() => handleClickEdit(infoCard)}
+        ></EditButton>
+        <h3>BUDGETS:</h3>
+        {infoCard.budgets &&
+          infoCard.budgets.map((item) => {
+            return (
+              <div>
+                <p>
+                  {item.budget_category}: {item.budget_amount}
+                </p>
+              </div>
+            );
+          })}
+      </div>
+    );
+  };
+
   return (
     <div>
       <div>
-        <form onSubmit={(event) => handleSubmit(event)}>
-          <Grid item xs="auto">
-            <TextField
-              label="Trip Name:"
-              value={tripName}
-              onChange={(e) => setTripName(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs="auto">
-            <TextField
-              label="Amount"
-              value={budgetAmount}
-              onChange={(e) => setBudgetAmount(e.target.value)}
-            />
-            <TextField
-              label="Category"
-              value={budget}
-              onChange={(e) => setBudget(e.target.value)}
-            />
-          </Grid>
-          {budgetArray.length > 0
-            ? budgetArray.map((budget) => {
-                return (
-                  <div>
-                    <p>
-                      {budget.budget_category} {budget.budget_amount}
-                    </p>
-                  </div>
-                );
-              })
-            : "hello"}
-          <button type="submit">Set a budget</button>
-        </form>
-        <button onClick={() => handlePostInfo()}>Post info</button>
+        {clickEdit && (
+          <form
+            className="info-form-edit"
+            style={{ backgroundColor: "yellow" }}
+          >
+            <Grid>
+              <TextField
+                id="Trip Name"
+                color="secondary"
+                label={"Trip Name" + ":" + editCard.trip_name}
+                value={tripName}
+                onChange={(e) => setTripName(e.target.value)}
+              />
+            </Grid>
+          </form>
+        )}
       </div>
-      <div className="budgets-info-cards-container">
-        {infoCards > 0
-          ? infoCards.map((infoCard) => {
-              return (
-                <div>
-                  <p>Trip Name: {infoCard.trip_name}</p>
-                </div>
-              );
-            })
-          : null}
+      <div className="submit-Info-Form">
+        <button onClick={() => handlesubmitInfo()}>Submit Info</button>
+      </div>
+      <div className="info-cards-container">
+        {infoCards && infoCards.map((infoCard) => renderInfoCard(infoCard))}
       </div>
     </div>
   );
 };
+
+// Previous code
 
 export default SubmitBudgetPage;
