cfb82cbe61c48e286515f38abd73794daea441e5
Hawa BahBah <hawabahbah25@gmail.com>
2021-01-29 16:20:03 +0000

    index changed

--- server/index.js
+++ server/index.js
@@ -13,19 +13,27 @@ const app = express();
 app.use(cors());
 
 mongoose.Promise = global.Promise;
-mongoose
-  .connect(
-    process.env.MONGODB_URI,
-    //  ||
-    // `mongodb+srv://HawaBah:mongopassword25@cluster0.9kfx5.mongodb.net/black-codher-personal-project?retryWrites=true&w=majority`,
-    // `mongodb://localhost:27017/black-codher-personal-project`
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("MongoDB has been connected"))
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+// const conn = mongoose.connection;
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("MongoDB connection error: ", err);
+});
+// mongoose
+// .connect(
+//   process.env.MONGODB_URI,
+//   //  ||
+//   // `mongodb+srv://HawaBah:mongopassword25@cluster0.9kfx5.mongodb.net/black-codher-personal-project?retryWrites=true&w=majority`,
+//   // `mongodb://localhost:27017/black-codher-personal-project`
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// )
+// .then(() => console.log("MongoDB has been connected"))
+// .catch((err) => console.log(err));
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }));
