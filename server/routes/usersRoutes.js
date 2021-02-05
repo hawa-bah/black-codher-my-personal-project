f332b8681c9eff0e89ec701ce766b78c3361036c
Hawa BahBah <hawabahbah25@gmail.com>
2021-02-05 19:47:09 +0000

    added a catch() in users routes and deleted space in procfile

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -18,30 +18,32 @@ module.exports = (app) => {
     if (!isValid) {
       return res.status(400).json(errors);
     }
-    User.findOne({ email: req.body.email }).then((user) => {
-      if (user) {
-        return res.status(400).json({ email: "Email already exists" });
-      } else {
-        // creating new user
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-        });
-
-        // Hash password before saving in database
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then((user) => res.json(user))
-              .catch((err) => console.log(err));
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (user) {
+          return res.status(400).json({ email: "Email already exists" });
+        } else {
+          // creating new user
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
           });
-        });
-      }
-    });
+
+          // Hash password before saving in database
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser
+                .save()
+                .then((user) => res.json(user))
+                .catch((err) => console.log(err));
+            });
+          });
+        }
+      })
+      .catch((err) => console.log(err));
   });
 
   app.post(`/login`, (req, res) => {
@@ -53,40 +55,42 @@ module.exports = (app) => {
     const email = req.body.email;
     const password = req.body.password;
     // Find user by email
-    User.findOne({ email }).then((user) => {
-      // Check if user exists
-      if (!user) {
-        return res.status(404).json({ emailnotfound: "Email not found" });
-      }
-      // Check password
-      bcrypt.compare(password, user.password).then((isMatch) => {
-        if (isMatch) {
-          // Create JWT Payload (the playload is what will be included in the state saved )
-          const payload = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-          };
-          // Sign token
-          jwt.sign(
-            payload,
-            process.env.SECRET_KEY,
-            {
-              expiresIn: 31556926, // 1 year in seconds
-            },
-            (err, token) => {
-              res.json({
-                success: true,
-                token: "Bearer " + token,
-              });
-            }
-          );
-        } else {
-          return res
-            .status(400)
-            .json({ passwordincorrect: "Password incorrect" });
+    User.findOne({ email })
+      .then((user) => {
+        // Check if user exists
+        if (!user) {
+          return res.status(404).json({ emailnotfound: "Email not found" });
         }
-      });
-    });
+        // Check password
+        bcrypt.compare(password, user.password).then((isMatch) => {
+          if (isMatch) {
+            // Create JWT Payload (the playload is what will be included in the state saved )
+            const payload = {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+            };
+            // Sign token
+            jwt.sign(
+              payload,
+              process.env.SECRET_KEY,
+              {
+                expiresIn: 31556926, // 1 year in seconds
+              },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token,
+                });
+              }
+            );
+          } else {
+            return res
+              .status(400)
+              .json({ passwordincorrect: "Password incorrect" });
+          }
+        });
+      })
+      .catch((err) => console.log(err));
   });
 };
